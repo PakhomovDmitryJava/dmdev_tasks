4c4da63961ba411b41aa261ad00ce50d6862f0f4
”Dmitry <pakhomov.arz@gmail.com>
2022-09-13 16:49:56 +0300

    lesson 7 Hometask_alone3_1

---
+++ src/com/dmdev/hometask3/Hometask3_1.java
@@ -0,0 +1,40 @@
+package com.dmdev.hometask3;
+
+import com.dmdev.lesson7.hometask.Hometask1;
+
+/**
+ * Дан одномерный массив целых чисел.
+ * Написать функцию, удаляющую из него все отрицательные элементы
+ * (удалить - значит создать новый массив с только положительными элементами).
+ * После удаления - умножить каждый элемент массива на его длину.
+ * Например: [3, 5, -6, 3, 2, -9, 0, -123] -> [15, 25, 15, 10, 0]
+ * Не забывать, что всю логику приложения нужно выносить в отдельные функции. main - только для тестирования написанного функционала.
+ */
+public class Hometask3_1 {
+    public static void main(String[] args) {
+        int[] numbers = {3, 5, -6, 3, 2, -9, 0, -123,3, 5, -6, 3, 2, -9, 0, -1233, 5, -6, 3, 2, -9, 0, -1233, 5, -6, 3, 2, -9, 0, -123 };
+        Hometask1.printArray(creatingNewArray(numbers));
+    }
+
+    public static int positiveNumbersCounting(int[] array) {
+        int positiveNumbersCounter = 0;
+        for (int i : array) {
+            if (i >= 0) {
+                positiveNumbersCounter++;
+            }
+        }
+        return positiveNumbersCounter;
+    }
+
+    public static int[] creatingNewArray(int[] array) {
+        int resultArrayLength = positiveNumbersCounting(array);
+        int[] result = new int[resultArrayLength];
+        for (int i = 0, k = 0; i < array.length; i++) {
+            array[i] *= resultArrayLength;
+            if (array[i] >= 0) {
+                result[k++] = array[i];
+            }
+        }
+        return result;
+    }
+}
---
+++ src/com/dmdev/lesson7/hometask/Hometask1.java
@@ -0,0 +1,33 @@
+package com.dmdev.lesson7.hometask;
+
+/**
+ * Напишите программу, которая циклически сдвигает элементы массива вправо на одну позицию, и печатает результат.
+ * Цикличность означает, что последний элемент массива становится самым первым его элементом.
+ */
+public class Hometask1 {
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        arrayShift(values);
+    }
+
+    public static void arrayShift(int[] array) {
+        printArray(array);
+        int[] resultArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (i + 1  > array.length - 1) {
+                resultArray[0] = array[array.length - 1];
+            } else {
+                resultArray[i + 1] = array[i];
+            }
+
+        }
+        System.out.println();
+        printArray(resultArray);
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
