6c464ec78611ed0808eba17209d2290c597862f5
”Dmitry <pakhomov.arz@gmail.com>
2022-09-15 22:56:27 +0300

    Hometask4 was started

--- src/com/dmdev/hometask1/Hometask_alone1_1.java
+++ src/com/dmdev/hometask1/Hometask_alone1_1.java
@@ -1,9 +1,11 @@
 package com.dmdev.hometask1;
-/** В переменной minutes лежит число от 0 до 59.
-  * Написать функцию, которая принимает в качестве параметра значение переменной minutes и выводит на консоль
-  * в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
-  * Протестировать функцию в main.
- * */
+
+/**
+ * В переменной minutes лежит число от 0 до 59.
+ * Написать функцию, которая принимает в качестве параметра значение переменной minutes и выводит на консоль
+ * в какую четверть часа попадает это число (в первую, вторую, третью или четвертую).
+ * Протестировать функцию в main.
+ */
 
 public class Hometask_alone1_1 {
     public static void main(String[] args) {
@@ -11,15 +13,16 @@ public class Hometask_alone1_1 {
             quarterIdentifier(i);
         }
     }
+
     public static void quarterIdentifier(int minute) {
-       if (minute >= 0 && minute < 15) {
-           System.out.println(minute + " - this is a first quarter of an hour.");
-       } else if (minute >= 15 && minute < 30) {
+        if (minute >= 0 && minute < 15) {
+            System.out.println(minute + " - this is a first quarter of an hour.");
+        } else if (minute >= 15 && minute < 30) {
             System.out.println(minute + " - this is a second quarter of an hour.");
         } else if (minute >= 30 && minute < 45) {
-           System.out.println(minute + " - this is a third quarter of an hour.");
-       }else if (minute >= 45 && minute < 60) {
-           System.out.println(minute + " - this is a fourth quarter of an hour.");
-       }
+            System.out.println(minute + " - this is a third quarter of an hour.");
+        } else if (minute >= 45 && minute < 60) {
+            System.out.println(minute + " - this is a fourth quarter of an hour.");
+        }
     }
 }
