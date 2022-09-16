127bbf68ab85fa1f460cef44b9efb6530c8cdabc
”Dmitry <pakhomov.arz@gmail.com>
2022-09-16 16:04:53 +0300

    Hometask51

--- src/com/dmdev/oop/lesson10/hometask/Hometask1.java
+++ src/com/dmdev/oop/lesson10/hometask/Hometask1.java
@@ -5,10 +5,35 @@ package com.dmdev.oop.lesson10.hometask;
  */
 public class Hometask1 {
     public static void main(String[] args) {
-
+        String someString1 = "А роза упала на лапу Азора, , , , , ,,! !!!!!_______";
+        palindromeChecker(someString1);
+        String someString2 = "А вот он я, Лобов О.Б., Оля Нотова";
+        palindromeChecker(someString2);
     }
 
     public static void palindromeChecker(String value) {
 
+        String clearedValue = value.replace("!", "")
+                .replace(".", "")
+                .replace(",", "")
+                .replace("+", "")
+                .replace("-", "")
+                .replace("?", "")
+                .replace("_", "")
+                .replace(":", "")
+                .replace(" ", "")
+                .toLowerCase();
+        System.out.println(clearedValue);
+
+        StringBuilder sb = new StringBuilder(clearedValue);
+        String reversedAndClearedValue = sb.reverse().toString();
+        System.out.println(reversedAndClearedValue);
+
+        if (clearedValue.equals(reversedAndClearedValue)) {
+            System.out.println("This is a palindrome!");
+        } else {
+            System.out.println("This is NOT a palindrome!");
+        }
     }
+
 }
