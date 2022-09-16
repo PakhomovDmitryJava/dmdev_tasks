e7defbbf38e9537b4c9033b0659403e967f20964
”Dmitry <pakhomov.arz@gmail.com>
2022-09-16 16:05:05 +0300

    Hometask51

--- src/com/dmdev/hometask5/HometaskAlone51.java
+++ src/com/dmdev/hometask5/HometaskAlone51.java
@@ -24,13 +24,13 @@ public class HometaskAlone51 {
     public static void stringCleaner(String value) {
         StringBuilder sb = new StringBuilder();
         String deleteSpaces = value.replace(" ", "").toUpperCase();
-        for (int i = 0; i < deleteSpaces.length()-1 ; i++) {
-            if (deleteSpaces.charAt(i) == deleteSpaces.charAt(i+1)) {
-                deleteSpaces = deleteSpaces.replace(deleteSpaces.charAt(i) + Character.toString(deleteSpaces.charAt(i+1)), Character.toString(deleteSpaces.charAt(i)));
+        for (int i = 0; i < deleteSpaces.length() - 1; i++) {
+            if (deleteSpaces.charAt(i) == deleteSpaces.charAt(i + 1)) {
+                deleteSpaces = deleteSpaces.replace(deleteSpaces.charAt(i) + Character.toString(deleteSpaces.charAt(i + 1)), Character.toString(deleteSpaces.charAt(i)));
             }
-            for (int k = 0; k < deleteSpaces.length()-1 ; k++) {
-                if (deleteSpaces.charAt(k) == deleteSpaces.charAt(k+1)) {
-                    deleteSpaces = deleteSpaces.replace(deleteSpaces.charAt(k) + Character.toString(deleteSpaces.charAt(k+1)), Character.toString(deleteSpaces.charAt(k)));
+            for (int k = 0; k < deleteSpaces.length() - 1; k++) {
+                if (deleteSpaces.charAt(k) == deleteSpaces.charAt(k + 1)) {
+                    deleteSpaces = deleteSpaces.replace(deleteSpaces.charAt(k) + Character.toString(deleteSpaces.charAt(k + 1)), Character.toString(deleteSpaces.charAt(k)));
                 }
             }
         }
