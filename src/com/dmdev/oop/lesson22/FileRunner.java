d71de51bea58d4897f2c803033adb78854984670
”Dmitry <pakhomov.arz@gmail.com>
2022-10-26 21:24:12 +0300

    lesson22

--- src/com/dmdev/oop/lesson22/FileRunner.java
+++ src/com/dmdev/oop/lesson22/FileRunner.java
@@ -6,5 +6,7 @@ public class FileRunner {
     public static void main(String[] args) {
         File file = new File("resources/test.txt");
         System.out.println(file.exists());
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
     }
 }
